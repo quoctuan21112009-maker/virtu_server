db5a183856ca01f0ee24cb94dd14043e03f760b6
ChuQuocTuan <quoctuan21112009@gmail.com>
2026-08-19 16:48:50 +0700

    Add React App component (move your React code here)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+// COPY nội dung của app.jsx vào đây
+
+export default function App() {
+  return (
+    <div>
+      <h1>Virtu - Lớp Học Ảo</h1>
+      {/* Component của bạn sẽ ở đây */}
+    </div>
+  )
+}
